a48884f30c11c245bafac35b5c92d09d5599d7d8
CheloCr <xchellong18@gmail.com>
2023-03-28 00:40:41 -0600

    Random number done

--- game.js
+++ game.js
@@ -1,1 +1,12 @@
+//todo DOM ELEMENTS
+const buttonAgain = document.querySelector(".again");
+const titleText = document.querySelector(".titleText");
+const secretBox = document.querySelector(".secretNumber");
+const inputBox = document.querySelector(".number");
+const checkButton = document.querySelector(".check");
+const scoreTxt = document.querySelector(".score");
+const highScoreText = document.querySelector(".highScore");
 
+//todo Random number generator 1 to 20
+let randomNumber = Math.trunc(Math.random() * 20);
+secretBox.textContent = randomNumber; //! solo de prueba para ver #
